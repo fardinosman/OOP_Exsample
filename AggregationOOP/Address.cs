c4b608710450bffcb75b15e80b30fbb8f30c0c85
Fardin Osman <osmanfardin@hotmail.dk>
2021-07-09 12:47:52 +0200

    Aggregation L V

---
+++ AggregationOOP/Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AggregationOOP
+{
+    public class Address
+    {
+        public Address()
+        {
+
+        }
+        public String Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Pincode { get; set; }
+        
+        public void GetAddress()
+        {
+            Console.WriteLine($"Street={this.Street} \n City={this.City} \n State={this.State} \n PinCode={this.Pincode}");
+        }
+    }
+}
--- AggregationOOP/Program.cs
+++ AggregationOOP/Program.cs
@@ -10,6 +10,34 @@ namespace AggregationOOP
     {
         static void Main(string[] args)
         {
+            Student stuObj = new Student();
+            stuObj.Name = "Fardin Osman";
+            stuObj.roll_No = 111222;
+            stuObj.Class = 12;
+
+            Address addObj = new Address();
+            addObj.City = "Kolding";
+            addObj.Pincode = "6000";
+            addObj.State = "Sydjyland";
+            stuObj.Get_Student_Info(addObj);
+
+        }
+    }
+    class Student
+    {
+        public Student()
+        {
+
+
+        }
+        public string Name { get; set; }
+        public int roll_No { get; set; }
+        public int Class { get; set; }
+
+        public void Get_Student_Info(Address Obj)
+        {
+            Console.WriteLine($"Student Name={this.Name} \n Roll_No={this.roll_No} \n Class={this.Class}\n");
+            Obj.GetAddress();
         }
     }
 }
